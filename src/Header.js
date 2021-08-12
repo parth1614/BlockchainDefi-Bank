e4681adf5857414f6c2367ddade2c025d28f17a4
Parth Arora <60436959+parth1614@users.noreply.github.com>
2021-08-12 11:55:43 +0000

    Initial commit

---
+++ src/Header.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import SearchOutlined from "@material-ui/icons/SearchOutlined";
+import Logo from "./marine.svg";
+import './Header.css';
+
+function Header(){
+  return(
+   <div className="header_wrapper">
+
+            <div className="header_logo">
+                <img src={Logo} width={50} alt={"Allegory,The Defi Bank"} />
+            </div>
+
+            <div className="header_search">
+
+                <div className="header_searchContainer">
+                    <SearchOutlined />
+                    <input placeholder="The Allegory Defi Bank" type="text"/>
+
+                </div>
+
+            </div>
+
+            <div className="header_menuItems">
+
+                <a  href="#">Allegory Stocks</a>
+                <a   href="#">LiteraTi Portfolio</a>
+                <a   href="#">Cash Rules</a>
+                <a   href="#">Allegory Alerts!</a>
+                <a   href="#">Allegory Account</a>
+
+            </div>
+
+        </div>
+  );
+}
+
+export default Header;
